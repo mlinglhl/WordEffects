faff6809028bdf6a4c32a073d244634417dc7847
mlinglhl <minhung.ling@gmail.com>
2017-01-09 17:12:22 -0500

    Initial Commit

---
+++ Jan 9 Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Jan 9 Word Effects
+//
+//  Created by Minhung Ling on 2017-01-09.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
